0b3cf6dd4c9ee90fd6e4b436add452dcf1c46030
pinkklss <shaker_04@mail.ru>
2024-06-04 13:25:30 +0700

    Update module_2_5.py

--- module_2_5.py
+++ module_2_5.py
@@ -5,7 +5,7 @@ def get_matrix(n, m, value):
         matrix.append(name)
         for j in range(m):
             name.append(value)
-            return matrix
+    return matrix
 
 
 result1 = get_matrix(2, 2, 10)
